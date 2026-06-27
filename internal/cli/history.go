7dfe5535e96a09860e3807b39276c73a7819cf99
ashutosh229 <akumarjha875@gmail.com>
2026-06-27 12:36:25 +0530

    feat: migrate the default history file name to a const variable

--- internal/cli/history.go
+++ internal/cli/history.go
@@ -5,11 +5,13 @@ import (
 	"path/filepath"
 )
 
+const defaultHistoryFile = ".authcli_history"
+
 func historyFile() string {
 	home, err := os.UserHomeDir()
 	if err != nil {
 		return ""
 	}
 
-	return filepath.Join(home, ".authcli_history")
+	return filepath.Join(home, defaultHistoryFile)
 }
